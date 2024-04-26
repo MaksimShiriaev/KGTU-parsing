b70c87e0d9f096aae924ae02f70ce72907aaafba
Burdenko Danil <125580474+MaksimShiriaev@users.noreply.github.com>
2024-04-26 17:49:06 +0200

    Add files via upload

--- Parsing/block_parsers.py
+++ Parsing/block_parsers.py
@@ -24,57 +24,6 @@ month_replacements = {
     'декабря': '12'
 }
 
-#Парсинг блока https://www.klgtu.ru/abitur/
-class Edu_programms_parser(BaseParser):
-    def __init__(self, time_sleep=False):
-        super().__init__()
-        self.url = 'https://www.klgtu.ru/abitur/'
-        self.pages = ['op', 'op-mag'] #Здесь страницы уже в виде списка
-        self.time_sleep = time_sleep 
-        self.topic_block = 'edu_programms'
-
-    #Сбор ссылок с главной страницы раздела
-    def parse_urls(self):
-        all_pages_urls = []
-        for page in self.pages:
-            cur_url = self.url + page + '?view=table'
-            src = self.read_site(cur_url)
-            soup = BeautifulSoup(src, "lxml")
-            one_page_urls = [item.get("href") for item in \
-                    soup.find_all("a", class_="article-program article-program--light")]
-            all_pages_urls.extend(one_page_urls)
-
-            if self.time_sleep is True:
-                time.sleep(random.randrange(1, 3))
-
-        res = ['https://www.klgtu.ru' + url for url in all_pages_urls]
-        return res
-
-    #Парсинг текста сайтов
-    def parse_text(self, url):
-        src = self.read_site(url)
-        soup = BeautifulSoup(src, "lxml")
-
-        header = None
-        headline_info = ",".join([item.text for item in soup.find_all(class_="program-headline__info")]) + "; "
-        apply_conditions = ", ".join([item.text for item in soup.find_all("div", class_="program-stats__group")]) + ". "
-
-        block_title = 'None'
-        block_text = soup.find_all("div", class_= "block-info mb-15")
-        block_content = 'None'
-
-        html = soup.find('main')
-        
-        res = headline_info + apply_conditions + block_content
-        res = self.regex_cleaning(res)
-
-        public_date = None
-        title = block_title
-
-        if self.time_sleep is True:
-            time.sleep(random.randrange(1, 3))
-        return block_title, res, public_date, str(html)
-
 
 
 
@@ -124,7 +73,7 @@ class Kgtu_news_parser(BaseParser):
 
         news_text = " ".join([item.text.strip() for item in soup.find_all("div", class_="editor editor--14 mb-30 mbm-15")])#.replace("\n", "")
         paragraphs = [paragraph.strip() for paragraph in news_text.split('\n') if paragraph.strip()]
-        news = " ;;".join(paragraphs)
+        news = " ;; ".join(paragraphs)
         html = soup.find('main')
 
         res = header + self.headers_sep + news
@@ -185,7 +134,7 @@ class Kgtu_anons_parser(BaseParser):
 
         news_text = " ".join([item.text.strip() for item in soup.find_all("div", class_="editor editor--14 mb-30 mbm-15")])#.replace("\n", "")
         paragraphs = [paragraph.strip() for paragraph in news_text.split('\n') if paragraph.strip()]
-        news = " ;;".join(paragraphs)
+        news = " ;; ".join(paragraphs)
 
         html = soup.find('main')
 
@@ -247,7 +196,7 @@ class Kgtu_obyava_parser(BaseParser):
 
         news_text = " ".join([item.text.strip() for item in soup.find_all("div", class_="editor editor--14 mb-30 mbm-15")])#.replace("\n", "")
         paragraphs = [paragraph.strip() for paragraph in news_text.split('\n') if paragraph.strip()]
-        news = " ;;".join(paragraphs)
+        news = " ;; ".join(paragraphs)
 
         html = soup.find('main')
 
@@ -259,118 +208,6 @@ class Kgtu_obyava_parser(BaseParser):
         if self.time_sleep is True:
             time.sleep(random.randrange(1, 3))
         return header, res, public_date, str(html)
-
-
-
-
-#Парсинг блока https://www.klgtu.ru/divisions/studencheskiy_gorodok/
-class Kgtu_studgorodok_parser(BaseParser):
-    def __init__(self, time_sleep=False):
-        super().__init__()
-        self.url = 'https://www.klgtu.ru/divisions/studencheskiy_gorodok/'
-        self.time_sleep = time_sleep 
-        self.topic_block = 'studgorodok'
-
-    #Сбор ссылок с главной страницы раздела
-    def parse_urls(self):
-        all_pages_urls = []
-
-        cur_url = self.url + '?view=table'
-        src = self.read_site(self.url)
-        soup = BeautifulSoup(src, "lxml")
-        one_page_urls_1 = [item.get("href") for item in \
-                soup.find_all("a", class_="link-download mb-20 mbm-15")]
-        one_page_urls_2 = [item.get("href") for item in \
-                soup.find_all("a", class_="link-download mb-40 mbm-25")]
-        all_pages_urls.extend(one_page_urls_1)
-        all_pages_urls.extend(one_page_urls_2)
-
-        if self.time_sleep is True:
-            time.sleep(random.randrange(1, 3))
-
-        res = ['https://www.klgtu.ru' + url for url in all_pages_urls]
-        return res
-
-    #Парсинг текста сайтов
-    def parse_text(self, url):
-        src = self.read_site(url)
-        soup = BeautifulSoup(src, "lxml")
-
-        header = None
-        headline_info = ",".join([item.text for item in soup.find_all(class_="program-headline__info")]) + "; "
-        apply_conditions = ", ".join([item.text for item in soup.find_all("div", class_="program-stats__group")]) + ". "
-
-        block_title = 'None'
-        block_text = soup.find_all("div", class_= "block-info mb-15")
-        block_content = 'None'
-
-        html = soup.find('main')
-        
-        res = headline_info + apply_conditions + block_content
-        res = self.regex_cleaning(res)
-
-        public_date = None
-        title = block_title
-
-        if self.time_sleep is True:
-            time.sleep(random.randrange(1, 3))
-        return block_title, res, public_date, str(html)
-
-
-
-
-#Парсинг блока https://klgtu.ru/divisions/tsentr_professionalnogo_razvitiya_i_karery/
-class Kgtu_praktika_parser(BaseParser):
-    def __init__(self, time_sleep=False):
-        super().__init__()
-        self.url = 'https://klgtu.ru/divisions/tsentr_professionalnogo_razvitiya_i_karery/'
-        self.time_sleep = time_sleep 
-        self.topic_block = 'praktika'
-
-    #Сбор ссылок с главной страницы раздела
-    def parse_urls(self):
-        all_pages_urls = []
-
-        cur_url = self.url + '?view=table'
-        src = self.read_site(self.url)
-        soup = BeautifulSoup(src, "lxml")
-        one_page_urls_1 = [item.get("href") for item in \
-                soup.find_all("a", class_="link-download mb-20 mbm-15")]
-        one_page_urls_2 = [item.get("href") for item in \
-                soup.find_all("a", class_="link-download mb-40 mbm-25")]
-        all_pages_urls.extend(one_page_urls_1)
-        all_pages_urls.extend(one_page_urls_2)
-
-        if self.time_sleep is True:
-            time.sleep(random.randrange(1, 3))
-
-        res = ['https://www.klgtu.ru' + url for url in all_pages_urls]
-        return res
-
-    #Парсинг текста сайтов
-    def parse_text(self, url):
-        src = self.read_site(url)
-        soup = BeautifulSoup(src, "lxml")
-
-        header = None
-        headline_info = ",".join([item.text for item in soup.find_all(class_="program-headline__info")]) + "; "
-        apply_conditions = ", ".join([item.text for item in soup.find_all("div", class_="program-stats__group")]) + ". "
-
-        block_title = 'None'
-        block_text = soup.find_all("div", class_= "block-info mb-15")
-        block_content = 'None'
-
-        html = soup.find('main')
-        
-        res = headline_info + apply_conditions + block_content
-        res = self.regex_cleaning(res)
-
-        public_date = None
-        title = block_title
-
-        if self.time_sleep is True:
-            time.sleep(random.randrange(1, 3))
-        return block_title, res, public_date, str(html)
 
 
 
@@ -411,7 +248,7 @@ class Kgtu_instituts_parser(BaseParser):
         news_2 = "; ".join([item.text.strip() for item in soup.find_all("div", class_="wrapper wrapper--45-40 wrapper--mob-0-0")]).replace("\n\n\n\n", ";")
         news_3 = "; ".join([item.text.strip() for item in soup.find_all("div", class_="columns__col columns__col--4")])
 
-        news = " ;;".join([news_1, news_2, news_3]).strip()
+        news = " ;; ".join([news_1, news_2, news_3]).strip().replace("Контакты", "Контакты:").replace("email", "email -").replace("адрес", "адрес -")
 
         html = soup.find('main')
         
@@ -473,7 +310,7 @@ class Kgtu_news_en_parser(BaseParser):
 
         news_text = " ".join([item.text.strip() for item in soup.find_all("div", class_="editor editor--14 mb-30 mbm-15")])#.replace("\n", "")
         paragraphs = [paragraph.strip() for paragraph in news_text.split('\n') if paragraph.strip()]
-        news = " ;;".join(paragraphs)
+        news = " ;; ".join(paragraphs)
 
         html = soup.find('main')
 
@@ -485,61 +322,6 @@ class Kgtu_news_en_parser(BaseParser):
         if self.time_sleep is True:
             time.sleep(random.randrange(1, 3))
         return header, res, public_date, str(html)
-
-
-
-
-#Парсинг блока https://www.klgtu.ru/en/applicants/
-class Edu_programms_en_parser(BaseParser):
-    def __init__(self, time_sleep=False):
-        super().__init__()
-        self.url = 'https://www.klgtu.ru/en/applicants/'
-        self.pages = ['op', 'op-mag'] #Здесь страницы уже в виде списка
-        self.time_sleep = time_sleep 
-        self.topic_block = 'edu_programms_en'
-
-    #Сбор ссылок с главной страницы раздела
-    def parse_urls(self):
-        all_pages_urls = []
-
-        for page in self.pages: 
-            cur_url = self.url + page + '?view=table'
-            src = self.read_site(cur_url)
-            soup = BeautifulSoup(src, "lxml")
-            one_page_urls = [item.get("href") for item in \
-                    soup.find_all("a", class_="article-program article-program--light")]
-            all_pages_urls.extend(one_page_urls)
-
-            if self.time_sleep is True:
-                time.sleep(random.randrange(1, 3))
-
-        res = ['https://www.klgtu.ru' + url for url in all_pages_urls]
-        return res
-
-    #Парсинг текста сайтов
-    def parse_text(self, url):
-        src = self.read_site(url)
-        soup = BeautifulSoup(src, "lxml")
-
-        header = None
-        headline_info = ",".join([item.text for item in soup.find_all(class_="program-headline__info")]) + "; "
-        apply_conditions = ", ".join([item.text for item in soup.find_all("div", class_="program-stats__group")]) + ". "
-
-        block_title = 'None'
-        block_text = soup.find_all("div", class_= "block-info mb-15")
-        block_content = 'None'
-
-        html = soup.find('main')
-        
-        res = headline_info + apply_conditions + block_content
-        res = self.regex_cleaning(res)
-
-        public_date = None
-        title = block_title
-
-        if self.time_sleep is True:
-            time.sleep(random.randrange(1, 3))
-        return block_title, res, public_date, str(html)
 
 
 
@@ -579,7 +361,7 @@ class Kgtu_instituts_en_parser(BaseParser):
         news_1 = "; ".join([item.text.strip() for item in soup.find_all("div", class_="wrapper wrapper--45-40 wrapper--grey-light wrapper--radius wrapper--mob-0-0")]).replace("\n\n\n\n", ";")
         news_2 = "; ".join([item.text.strip() for item in soup.find_all("div", class_="wrapper wrapper--45-40 wrapper--mob-0-0")]).replace("\n\n\n\n", ";")
 
-        news = " ;;".join([news_1, news_2]).strip()
+        news = " ;; ".join([news_1, news_2]).strip().replace("Contacts", "Contacts:").replace("phone", "phone -").replace("email", "email -").replace("address", "address -")
 
         html = soup.find('main')
         
